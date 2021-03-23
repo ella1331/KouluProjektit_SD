8dbf2743efbd4085ebc9396a3093b744363fb201
Toni Kuukkanen <tonikuukkanen91@gmail.com>
2021-03-23 15:54:52 +0200

    Lisäsin vähän functiota kokeilu mielessä, ihan if else. Lisäsin huvikseen google fonteista linkkiä tyyleihin ja määritin diville nimen art. Koodin sekaan lisätty kommentit mitä muuta kokeilin. Tarkistaa nyt etunimen ja sukunimen. Kaikki vapaasti muokattavissa.

---
+++ projekti1/projekti1a.js
@@ -0,0 +1,20 @@
+//Tonin ehdotusta, laittakee omaa kommenttia ja muokatkaa vapaasti
+
+function tarkista() {
+    var etunimi = document.forms["kysely"]["fname"].value;
+    var sukunimi = document.forms["kysely"]["lname"].value;
+    if (etunimi == "") {
+      alert("Laittakee etunimi");
+      return false;
+    }
+    else if (sukunimi == "") {
+            alert("Laittakee sukunimi");
+            return false;
+        }
+
+    else {
+        alert("Tietonne on lähetetty");
+        return true;
+    }
+  }
+
