96e4ac77a207da42f818ecdd1105e459355003aa
Toni Kuukkanen <tonikuukkanen91@gmail.com>
2021-03-30 22:45:45 +0300

    Javascriptiin alustettu nuo muuttujat valmiiksi, sori en malttanut :D /Toni/30.3.21

---
+++ YksinkertainenKysely/js/script.js
@@ -0,0 +1,12 @@
+function lahetaVastaukset(){
+    var yhteensa = 5;
+    var pisteet = 0;
+    var huom = 0;
+
+    //Hae käyttäjän syötteet
+    var k1 = document.forms["kyselyLomake"]["k1"].value;
+    var k2 = document.forms["kyselyLomake"]["k2"].value;
+    var k3 = document.forms["kyselyLomake"]["k3"].value;
+    var k4 = document.forms["kyselyLomake"]["k4"].value;
+    var k5 = document.forms["kyselyLomake"]["k5"].value;
+}
