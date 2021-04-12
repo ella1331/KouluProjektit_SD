6586dcb78322ffcdd4af92cb6f5833e70d9fb53b
Toni Kuukkanen <tonikuukkanen91@gmail.com>
2021-04-12 13:07:37 +0300

    Korjattu Jyrin huomaamat virheet, kommentoitu tehdyt muutokset koodiin, pieniä korjauksia\Toni\12.4

--- projekti1/projekti.js
+++ projekti1/projekti.js
@@ -1,40 +1,27 @@
 function tarkista() {
-var kokoNimi = document.getElementById('nimi').value;
-var maili = document.getElementById("email").value;
-var palvelunkieli = document.getElementById('kieli').value;
-var valinta = document.getElementById('palvelu').checked;
-var laatikko = document.getElementById('alue').value;
+  var kokoNimi = document.getElementById("nimi").value;
+  var maili = document.getElementById("email").value;
+  var palvelunkieli = document.getElementById("kieli").value;
+  var laatikko = document.getElementById("alue").value;
 
-if (kokoNimi == "") {
-  alert ("Laittakkee nimi");
-  return false;
-}
-
-else if (maili == "") {
-  alert ("Laittakkee sähköposti");
-  return false;
-}
-
-else if (palvelunkieli == "" ) {
-  alert ("Jottain kieltä pittää valita");
-  return false;
-}
-
-else if (valinta == "") {
-  alert ("Mittee palavelluu laitettaan");
-  return false;
-}
-
-else if (laatikko == "") {
-  alert ("Lissee informaatioo");
-  return false;
-}
-
-else {
-  alert ("Teitä palavellaan ihan kohta");
-  return true;
-}
-
-
-
+  if (kokoNimi == "") {
+    alert("Laittakkee nimi");
+    return false;
+  } else if (maili == "") {
+    alert("Laittakkee sähköposti");
+    return false;
+  } else if (palvelunkieli == "") {
+    alert("Jottain kieltä pittää valita");
+    return false;
+  } else if (document.querySelector("input[name='palvelua']:checked") == null) {
+    //Käytin tässä metodia queryselector joka hakee input, nimellä palvelua, ja tarkistaa että checked ja jos se on null eli tyhjä/false, alerttaa ton valitkee palvelu/Toni/12.4
+    alert("Valitkee palvelu");
+    return false;
+  } else if (laatikko == "") {
+    alert("Lissee informaatioo");
+    return false;
+  } else {
+    alert("Teitä palavellaan ihan kohta");
+    return true;
+  }
 }
--- projekti2/projekti2.js
+++ projekti2/projekti2.js
@@ -1,55 +1,29 @@
-var arvattava = Math.floor(Math.random()*10)+1;
-var kerrat = 0;
+var arvattava = Math.floor(Math.random() * 10) + 1;
+var kerrat = 1; //Muutin tämän 1:ksi, niin nyt laskee oikein arvauskerrat/Toni/12.4
 
+function arvaa(arpa) {
+  var x = arpa.value;
 
-
-function arvaa(arpa){
-      
-       
-    var x = arpa.value;
-    
-    if(kerrat <= 2){
-   
-    if(x == arvattava)
-    {    
-        alert("Onnea sulle, oikeempa valitsit!"
-                + " Arvauskertoja: " + kerrat + " Peli alkaa alusta!");
-                location.reload(); //Sivuston automaattinen päivitys (Flex)
-                
-                
+  if (kerrat <= 3) {
+    if (x == arvattava) {
+      alert(
+        "Onnea sulle, oikeempa valitsit!" +
+          " Arvauskertoja: " +
+          kerrat +
+          " Peli alkaa alusta!"
+      );
+      location.reload(); //Sivuston automaattinen päivitys (Flex)
+    } else if (x > arvattava) {
+      kerrat++;
+      alert("On se kyl vähä pienempi");
+      arpa.style.display = "none"; //Lisätty arvatun numeron poisto
+    } else if (x < arvattava) {
+      kerrat++;
+      alert("Kokkeile isompaa hyvä immeinen");
+      arpa.style.display = "none"; //Lisätty arvatun numeron poisto
     }
-    else if(x > arvattava) 
-    {    
-        kerrat++;
-        alert("On se kyl vähä pienempi");
-        arpa.style.display = "none"; //Lisätty arvatun numeron poisto
-    }
-    else if (x < arvattava)
-    {
-        kerrat++;
-        alert("Kokkeile isompaa hyvä immeinen");
-        arpa.style.display = "none"; //Lisätty arvatun numeron poisto
-    }
-    
-}
-
-
-
-else {
+  } else {
     alert("Game over! Peli alakaa alusta");
-location.reload(); //Laitettiin sivuston päivitys automaattiseksi, powered by Google
+    location.reload(); //Laitettiin sivuston päivitys automaattiseksi, powered by Google
+  }
 }
-
-}
-
-
-    
-
-
-    
-
-
-
-
-
-
