58701917db887f6337de189fba17c54831c69404
Toni Kuukkanen <tonikuukkanen91@gmail.com>
2021-05-17 13:41:28 +0300

    Tehty pikku muokkailuja ja tehty listaa() funktio, muuta tarvittaessa, jos jokin ei miellytä :D Ja oli nopea homma, pikku hifistelyyn meni suurin osa ajasta/Toni 17.5

--- projekti6/projekti6.js
+++ projekti6/projekti6.js
@@ -1,15 +1,10 @@
-let tallennaButton = document.getElementById('painike');
 let resetButton = document.getElementById('reset');
 let form = document.getElementById('kyselyLomake');
 
-//functio kyselylomakkeelle
-form.addEventListener("submit" , function (e) {
-  e.preventDefault();
-}
-)
+
 
 //tallentaa lokaalisti
-tallennaButton.addEventListener("click", function(e)
+form.addEventListener("submit", function(e)
 {
   let etu = document.getElementById('etunimi');
   let suku = document.getElementById('sukunimi');
@@ -39,6 +34,10 @@ tallennaButton.addEventListener("click", function(e)
 
   maili = maili.value;
   localStorage.setItem('maili', maili);
+  //Lisätty tyhjennys funktio joka tyhjentää formin tallennuksen jälkeen, myöskin ilmoitus tallennuksesta lisätty/Toni 17.5
+  tyhjennys();
+  alert("Tietosi on tallennettu");
+
 }
 )
 
@@ -66,4 +65,44 @@ resetButton.addEventListener ("click", function ()
 // tyhjentää local storagen
 function deleteItems(){
   localStorage.clear();
+  location.reload();
+  //Lisätty sivun päivitys, niin haetut tiedot häipyvät myös pois näkyvistä/Toni 17.5
+}
+
+//Hakee tallennetut tiedot /Toni 17.5
+function listaa(){
+  document.getElementById("otsikkoLaatikko").innerHTML = "Tallennetut tiedot:";
+  let etuNimi = localStorage.getItem('etunimi');
+  document.getElementById("retunimi").innerHTML = "Etunimi: "+ etuNimi;
+  let sukuNimi = localStorage.getItem('sukunimi');
+  document.getElementById("rsukunimi").innerHTML = "Sukunimi: "+ sukuNimi;
+  let osoite = localStorage.getItem('lähiosoite');
+  document.getElementById("rosoite").innerHTML = "Osoite: "+ osoite;
+  let postiNumero = localStorage.getItem('postinumero');
+  document.getElementById('rpostinumero').innerHTML = "Postinumero: "+ postiNumero;
+  let postiPaikka = localStorage.getItem('postiToimiPaikka');
+  document.getElementById('rpostipaikka').innerHTML = "Postitoimipaikka: "+ postiPaikka;
+  let puhelin = localStorage.getItem('puh');
+  document.getElementById('rpuhelin').innerHTML = "Puhelin: "+ puhelin;
+  let email = localStorage.getItem('maili');
+  document.getElementById("remail").innerHTML = "Email: "+ email;
+  
+}
+//Tallennettaessa kutsuu tätä funktiota, tyhjentää lomakkeen tallennuksen jälkeen/Toni/17.5
+function tyhjennys(){
+  let etu = document.getElementById('etunimi');
+  let suku = document.getElementById('sukunimi');
+  let lähi = document.getElementById('lähiosoite');
+  let posti = document.getElementById('postinumero');
+  let paikka = document.getElementById('postiToimiPaikka');
+  let puh = document.getElementById('puh');
+  let maili = document.getElementById('maili');
+
+  etu.value = '';
+  suku.value = '';
+  lähi.value = '';
+  posti.value = '';
+  paikka.value = '';
+  puh.value = '';
+  maili.value = '';
 }
